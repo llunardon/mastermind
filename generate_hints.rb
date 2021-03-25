6e09056c57142591b0811f8513bc627773bf1798
llunardon <lorenzo.lunardon.1@gmail.com>
2021-03-25 23:56:20 +0100

    working prototype

--- generate_hints.rb
+++ generate_hints.rb
@@ -1,8 +1,10 @@
-def generate_code(n)
-  possible_colors = ["black", "white", "red", "blue", "yellow", "green", "orange", "purple"]
-  puts "possible colors: " + possible_colors.join(', ')
+# frozen_string_literal: true
+
+def generate_code(num)
+  possible_colors = %w[black white red blue yellow green orange purple]
+  puts "possible colors: #{possible_colors.join(', ')}"
   code = []
-  until code.length == n do
+  until code.length == num
     code.push(color = possible_colors.sample)
     possible_colors.delete(color)
   end
@@ -15,13 +17,14 @@ def generate_hints(code, guess)
 
   guess.uniq.each_with_index do |color, index|
     if color == code[index]
-      hints.push("green")
-    elsif code.include?(color) 
-      hints.push("white")
+      hints.push('green')
+    elsif code.include?(color)
+      hints.push('white')
     end
   end
 
-  puts 'hints: ' + hints.join(', ')
+  puts "hints: #{hints.join(', ')}"
+  puts ''
   if right_guess?(hints, code.length)
     puts 'You won! Congratulations.'
     exit
@@ -29,24 +32,33 @@ def generate_hints(code, guess)
 end
 
 def right_guess?(hints, len)
-  (hints.uniq)[0] = "green" && hints.length == len && hints.uniq.length == 1
+  hints.include?('green') && hints.length == len && hints.uniq.length == 1
 end
 
-def get_guess(n)
+def get_guess(num)
   puts 'Insert your guess: '
-  until (guess = gets.chomp.split(' ')).length == n
-    #puts 'Try to guess the four-color code. Use WHITESPACES to delimit words, and watch for TYPOS! '
+  puts ''
+  until (guess = gets.chomp.split(' ')).length == num
+    # puts 'Try to guess the four-color code. Use WHITESPACES to delimit words, and watch for TYPOS! '
     puts 'Insert your guess: '
   end
 
   guess
 end
 
-puts '*******************************************************************************************'
-puts '*Try to guess the four-color code. Use WHITESPACES to delimit words, and watch for TYPOS! *'
-puts '*******************************************************************************************'
-code = generate_code(4)
-p code
-(1..12).each do
-  generate_hints(code, get_guess(4))
+def play_game
+  puts '*******************************************************************************************'
+  puts '*Try to guess the four-color code. Use WHITESPACES to delimit words, and watch for TYPOS! *'
+  puts '*The code doesn\'t have repeated colors!                                                   *'
+  puts '*Hints legend:                                                                            *'
+  puts '* green: right color and right position                                                   *'
+  puts '* white: right color and wrong position                                                   *'
+  puts '*******************************************************************************************'
+  code = generate_code(4)
+  #p code
+  12.times do
+    generate_hints(code, get_guess(4))
+  end
 end
+
+play_game
